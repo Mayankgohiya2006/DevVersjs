4e516cfd4c230f574ad6da56b5f5e33e20b85789
Mayankgohiya2006 <mayankkgohiya111@gmail.com>
2025-11-10 17:05:27 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,218 @@
+
+console.log("---1---");
+
+// let num = 10;
+
+// if(num >0){
+//     console.log(num);
+// }else if(num === 0){
+//     console.log(num);
+// }else{
+//     console.log(num);
+// }
+
+console.log("---2---");
+
+// function findAge(age){
+//        if(age >= 18){
+//         console.log("Adult",age);
+//        }else{
+//         console.log("Minor",age);
+//        }
+// }
+
+// findAge(17);
+
+
+console.log("---3---");
+
+// function givenNumber(num){
+//     if(num % 2 == 0){
+//         console.log(num," is Even Number");
+//     }else{
+//         console.log(num,"is Odd Number");
+//     }
+// }
+
+
+console.log("---4---");
+
+// function numberCheck(num1,num2){
+//     if(num1 > num2){
+//         console.log("Number 1 is greater :",num1);
+//     }else{
+//         console.log("Number 2 is greater :",num2);
+//     }
+// }
+
+// numberCheck(4,7);
+
+
+
+console.log("---5---");
+
+//  function passCheck(password){
+//     if(password === "admin123"){
+//         console.log("Access");
+//     }else{
+//         console.log("Access denied");
+//     }
+//  }
+
+//  passCheck("man");
+
+
+
+
+console.log("---6---");
+
+// function yearFind(year){
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//   console.log("Leap year");
+// } 
+// else {
+//   console.log("Not a leap year");
+// }
+// }
+
+// yearFind(2000);
+
+
+
+console.log("---7---");
+
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// if(a > b && a > c){
+//     console.log("A is largest :",a);
+// }else if(b > c ){
+//     console.log("B is largest :",b);
+// }else{
+//     console.log("C is largest :",c);
+// }
+
+ 
+console.log("---8---");
+
+
+// function marksCheck(marks){
+//     if(marks >= 90){
+//         console.log("Grade A");
+//     }else if (marks >= 75){
+//         console.log("Grade B");
+//     }else if (marks >= 50){
+//         console.log("Grade C");
+//     }else{
+//         console.log("Grade F");
+//     }
+// }
+
+// marksCheck(50);
+
+
+
+console.log("---9---");
+
+// function charCheck(char){
+//     if(char === "a" || char === "e"|| char === "i" || char === "o" || char === "u"){
+//       console.log(char,"is Vowel");
+//     }else{
+//         console.log(char,"is Consonant")
+//     }
+// }
+
+// charCheck("l");
+
+
+
+console.log("---10---");
+
+// function checkTemp(Temprature){
+//     if(Temprature > 30){
+//         console.log(Temprature,"It's  To  Hot")
+//     }else if(Temprature >15 && Temprature === 30){
+//         console.log(Temprature,"Nice Weather");
+//     }else{
+//         console.log(Temprature,"It's  To  Cold");
+//     }
+// }
+
+// checkTemp(30);
+
+
+
+
+console.log("---11---");
+
+// let hours = Number(prompt("Enter your digit"));
+//  if(hours <= 5  ) {
+//       console.log("Night");
+// }
+//  else if(hours <= 10){
+//     console.log("Morning");
+// }else if(hours <= 16){
+//     console.log("Afternoon");
+// }
+// else if(hours <= 24 ){
+//     console.log("Evening");
+// }else if( hours >24){
+//     console.log("Invalid Time");
+// }
+
+
+console.log("---12---");
+
+// function accountCheck(userName ,password){
+
+// if(userName === "admin" && password ==="1234"){
+//     console.log('LOGIN SUCCESSFUL');
+// }else{
+//     console.log("INVALID CREDENTIALS");
+// }
+// }
+
+// accountCheck("admin", "1234");
+
+
+console.log("---14---");
+
+// function numberDivi(num){
+//     if(num %3 == 0 && num % 5 == 0){
+//         console.log("Both");
+//     }else if (num %3 ==0){
+//         console.log(" Divisibal by Only 3");
+//     }else if (num %5 == 0){
+//         console.log(" Divisibal by Only 5");
+//     }else{
+//         console.log("Neither");
+//     }
+// }
+
+// numberDivi(15);
+
+console.log("---15---");
+
+// function dayCheck(day){
+//     if(day === 1){
+//         console.log("Monday");
+//     }else if(day === 2){
+//         console.log("Tuesday");
+//     }else if(day === 3){
+//         console.log("Wednesday");
+//     }else if(day === 2){
+//         console.log("Thursday");
+//     }else if(day === 2){
+//         console.log("Friday");
+//     }else if(day === 2){
+//         console.log("Saturday");
+//     }else if(day === 2){
+//         console.log("Sunday");
+//     }else{
+//      console.log("Invalid number");
+//     }
+// }
