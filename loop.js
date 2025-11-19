4404585c671da6aecd428c343719fc08f4168602
Mayankgohiya2006 <mayankkgohiya111@gmail.com>
2025-11-19 10:38:21 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,108 @@
+//  console.log("---1---");
+
+// function printHello(){
+//     for(let i = 1; i <=3 ; i++){
+//         console.log("Hello");
+//     }
+// }
+
+// printHello();
+
+
+ console.log("---2---");
+
+
+// function printNumber(){
+//     for(let i =1; i <=10; i++){
+//         console.log(i);
+//     }
+// }
+
+// printNumber()
+
+ console.log("---3---");
+
+//  function printEven(){
+//     for(let i =1; i<=20; i++){
+//         if(i % 2 === 0){
+//             console.log(i,"Even");
+//         }
+//     }
+//  }
+
+//  printEven();
+
+ console.log("---4---");
+
+//  function yourName(name){
+//     for(let i =1; i <=5; i++){
+//         console.log(name);
+//     }
+//  }
+
+//  yourName("Mayank");
+
+ console.log("---5---");
+
+ 
+
+//  function sumOfAll(){
+//     let sum = 0;
+//     for(let i = 0;i <=100; i++){
+//        sum = sum + i;
+       
+//     }
+//        console.log(sum);
+
+//  }
+
+//  sumOfAll();
+
+ console.log("---6---");
+  
+
+//  function table(num){
+//     let result =0;
+//     for(let i =1; i <= 10 ; i++){
+//         result = num*i; 
+//          console.log(num, "X" , i, "=", result);
+//     }
+  
+//  }
+
+//  table(3);
+
+ console.log("---7---");
+
+//  let arr = [10,20,30,40,50,60,70,80,90,100];
+
+//  for(let i =0; i <arr.length ; i++){
+//     console.log(arr);
+//  }
+
+ console.log("---8---");
+
+// function chechNum(){
+//     for(let i =0; i <=num ; i++){
+//         if(num %3 ===0 && num % 5 ===0 ){
+//             console.log("divisible byonly 3 and 5 both",num)
+
+//         }else{
+//             console.log("Not Divisible");
+//         }
+//     }
+// } 
+// chechNum();
+
+ console.log("---9---");
+
+  
+// let myName = "Mayank";
+// let reverc = "";
+
+// for(let i = myName.length - 1 ; i >= 0; i--){
+//      reverc += myName[i];  
+// }
+
+
+// console.log(reverc);
